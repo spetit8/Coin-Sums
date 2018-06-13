a76ff7668b7054b814316d003207969b705074f1
SIM64 <38591068+SIM64@users.noreply.github.com>
2018-06-12 21:34:33 -0600

    Initial commit

---
+++ Coin Sums.py
@@ -0,0 +1,19 @@
+CoinValue = {'1p' : 1,
+             '2p' : 2,
+             '5p' : 5,
+             '10p' : 10,
+             '20p' : 20,
+             '50p' : 50,
+             '£1' : 100,
+             '£2' : 200}
+
+Total = 200
+
+while(True):
+    for Num£2 in range(Total/CoinValue['£2'] + 1):
+        TotalRemaining = 
+
+
+
+
+list = [1,2,3,4,5,6,7,8]
